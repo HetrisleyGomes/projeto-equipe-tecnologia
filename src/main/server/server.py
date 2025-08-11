548c6abcdd6ddfbb0588bb2c1a7724e1c6851347
Hetrisley Gomes <hetrisleygomes@gmail.com>
2025-08-11 11:00:10 -0300

    TEST: Removido cors do server.py.

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from flask_cors import CORS
+#from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
-CORS(app, origins=["*"])
-socketio = SocketIO(app, cors_allowed_origins=["https://registro-suporte-ma.onrender.com", "http://172.20.9.251:5000"])
+#CORS(app, origins=["*"])
+socketio = SocketIO(app)
+#cors_allowed_origins=["https://registro-suporte-ma.onrender.com", "http://172.20.9.251:5000"])
 
 @socketio.on("connect")
 def handle_connect():
