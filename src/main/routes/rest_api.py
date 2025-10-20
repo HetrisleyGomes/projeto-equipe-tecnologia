386f0618ca22a041199a25ed0e09c3718431a9d2
Hetrisley Gomes <hetrisleygomes@gmail.com>
2025-10-20 11:42:43 -0300

    FEAT: Correções com a API REST.

--- src/main/routes/rest_api.py
+++ src/main/routes/rest_api.py
@@ -1,11 +1,5 @@
 from flask import render_template, url_for, request, redirect, Blueprint, jsonify, send_file, g
-import json
-import csv
-import pandas as pd
-from io import StringIO, BytesIO
 import psycopg2
-
-from routes import get_db_connection
 
 from src.models.repositories.requisicoes_repository import RequisicoesRepository
 from src.controllers.requisicao_controller import RequisicaoController
@@ -19,6 +13,26 @@ from src.main.server.server import (
 
 
 rest_bp = Blueprint("rest_bp",__name__)
+
+# Função para conectar ao banco de dados
+def get_db_connection():
+    if 'db_conn' not in g:
+        try:
+            conn_string = db_connection_handler.get_connection_string()
+            g.db_conn = psycopg2.connect(conn_string)
+            print("Conexão ao banco PostgreSQL estabelecida para a requisição.")
+        except Exception as e:
+            print(f"Erro ao conectar no banco: {e}")
+            g.db_conn = None
+    return g.db_conn
+
+# Função para fechar a conexão no final de cada requisição
+@app.teardown_appcontext
+def close_db_connection(e=None):
+    db_conn = g.pop('db_conn', None)
+    if db_conn is not None:
+        db_conn.close()
+        print("Conexão com o banco fechada.")
 
 @rest_bp.route("/get", methods=["GET"])
 def get_all():
--- src/main/routes/routes.py
+++ src/main/routes/routes.py
@@ -20,26 +20,6 @@ inverter_ordem = False
 mostrar_prioridades = True
 mostrar_finalizados = False
 
-# Função para conectar ao banco de dados
-def get_db_connection():
-    if 'db_conn' not in g:
-        try:
-            conn_string = db_connection_handler.get_connection_string()
-            g.db_conn = psycopg2.connect(conn_string)
-            print("Conexão ao banco PostgreSQL estabelecida para a requisição.")
-        except Exception as e:
-            print(f"Erro ao conectar no banco: {e}")
-            g.db_conn = None
-    return g.db_conn
-
-# Função para fechar a conexão no final de cada requisição
-@app.teardown_appcontext
-def close_db_connection(e=None):
-    db_conn = g.pop('db_conn', None)
-    if db_conn is not None:
-        db_conn.close()
-        print("Conexão com o banco fechada.")
-
 @main_bp.route("/")
 @main_bp.route("/index")
 def index():
